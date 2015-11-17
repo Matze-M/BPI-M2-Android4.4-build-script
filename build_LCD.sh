1a5708b3081e3bed22342f29ee591e7af5898b7e
OtakuNekoP <otakunekop@52miku.com>
2015-11-17 09:02:20 +0800

    Create build_LCD.sh

---
+++ build_LCD.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# (c) 2015, Leo Xu <otakunekop@banana-pi.org.cn>
+# Build script for BPI-M2 Android 4.2 source code with kernel 3.3 2015.05.31
+
+rm ./lichee/tools/pack/chips/sun6i/configs/android/mars-a31s/sys_config.fex
+cp ./lichee/tools/pack/chips/sun6i/configs/android/mars-a31s/sys_config_7-touchscreen.fex ./lichee/tools/pack/chips/sun6i/configs/android/mars-a31s/sys_config.fex
+rm ./android/device/softwinner/fiber-common/egl/hwcomposer.sun6i.so
+cp ./android/device/softwinner/fiber-common/egl/hwcomposer.sun6i.7-touchscreen.so ./android/device/softwinner/fiber-common/egl/hwcomposer.sun6i.so
+cd ./lichee
+./build.sh -p sun6i_fiber_a31s
+cd ../android
+source build/envsetup.sh
+lunch mars_a31s-eng
+extract-bsp
+make -j8
+pack
+cd ../lichee/tools/pack
+ls -l
